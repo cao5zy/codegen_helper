cd18e5daba56e92be12e50220483cc18cf6ce6c9
Ubuntu <caozon@code-dev.4co5vkhwnyru5da0oj2ltjzlib.hx.internal.cloudapp.net>
2018-07-23 13:37:23 +0000

    0.0.9
    Signed-off-by: Ubuntu <caozon@code-dev.4co5vkhwnyru5da0oj2ltjzlib.hx.internal.cloudapp.net>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ requires = [],
 
 setup(
     name = name,
-    version = '0.0.8',
+    version = '0.0.9',
     author = 'Zongying Cao',
     author_email = 'zongying.cao@dxc.com',
     description = 'utilities for code-engine.',
