fbffbd595c047aafbc70dd35b805f07edc89cb56
Ubuntu <caozon@dev.tmljhwvwekuuzmg1zw4txphbgc.cx.internal.cloudapp.net>
2018-12-01 03:24:40 +0000

    increase version number
    Signed-off-by: Ubuntu <caozon@dev.tmljhwvwekuuzmg1zw4txphbgc.cx.internal.cloudapp.net>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ requires = [],
 
 setup(
     name = name,
-    version = '0.0.14',
+    version = '0.0.15',
     author = 'Zongying Cao',
     author_email = 'zongying.cao@dxc.com',
     description = 'utilities for code-engine.',
